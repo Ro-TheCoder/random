3156d0dce5233b528f139e003cb32ed1c9cd7552
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-10-15 18:45:21 +0530

    Update stack_one.py

--- stack_one.py
+++ stack_one.py
@@ -1,23 +1,35 @@
 import sys
 
 def create():
-	lst=[]
-	limit=int(input("enter the size of the stack to be created: "))
-	for i in range(limit):
-		lst.append(int(input("enter the element to be intered in stack: ")))
-	return lst
+    global lst
+    global limit
+    limit=int(input("enter the size of the stack to be created: "))
+    for i in range(limit):
+        lst.append(int(input("enter the element to be entered in stack: ")))
+def push():
+    global lst
+    if len(lst)<limit:
+        lst.append(int(input("enter the element to be entered in stack: ")))
+    else: print("stack overflow")
+
+def pop():
+    global lst
+    lst=lst[0:len(lst)-1]
+
+def display():
+    global lst
+    print(lst)
+    
+def main():
+    while True:
+        print("what do you want to do?\n	1--> create new stack\n 2--> push to stack\n    3--> pop from stack \n	4--> display the stack \n 5-->  exit")
+        choice = int(input("	:: "))
+        func={1:create,2:push,3:pop,4:display,5:sys.exit}
+        if choice in func.keys():
+            func[choice]()
+        else: print("wrong choice")
 
 if __name__=="__main__":
-	while True:
-		print("what do you want to do?\n	1--> create the stack\n	2--> pop from stack\n	3--> display the stack\n	4--> exit")
-		choice=int(input("	:: "))
-		if choice==1:
-			lst=create()
-		elif choice==2:
-			if len(lst)
-			lst.pop()
-		elif choice==3:
-			print(lst)
-		elif choice==4:
-			sys.exit(0)
-		else:print("wrong choice")
+    limit = 0
+    lst = []
+    main()
